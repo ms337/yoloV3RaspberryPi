b9667a68c255738d43c09a71d120b7a928b4a93d
jeet undhad <jeetundhad@jeets-MacBook-Pro.local>
2019-10-27 19:59:22 -0400

    coordinate complete

--- Coordinate.cpp
+++ Coordinate.cpp
@@ -1,7 +1,21 @@
 #include "Coordinate.h"
 
-Coordinate::Coordinate(float x, float y)
-{
-  xCoord = x;
-  yCoord = y;
+Coordinate::Coordinate (float x , float y){
+  xCoord=x;
+  yCoord=y;
 };
+
+float Coordinate::getx(){
+  return xCoord;
+}
+
+float Coordinate::gety(){
+  return yCoord;
+}
+
+void Coordinate::setX(float x){
+  xCoord = x;
+}
+void Coordinate::setY(float y){
+  yCoord = y;
+}
