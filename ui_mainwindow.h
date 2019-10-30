7d3673be93b2899e3d042440a0c52d75305cdded
Madhav <msinghal@uwo.ca>
2019-10-30 15:03:10 -0400

    Linux CMake Fix for Qt

---
+++ ui_mainwindow.h
@@ -0,0 +1,105 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.5.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QListView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QListView *objectsList;
+    QListView *zonesList;
+    QLabel *object;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QWidget *camera;
+    QLabel *label;
+    QMenuBar *menuBar;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(825, 480);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        objectsList = new QListView(centralWidget);
+        objectsList->setObjectName(QStringLiteral("objectsList"));
+        objectsList->setGeometry(QRect(580, 0, 241, 411));
+        zonesList = new QListView(centralWidget);
+        zonesList->setObjectName(QStringLiteral("zonesList"));
+        zonesList->setGeometry(QRect(0, 291, 581, 121));
+        object = new QLabel(centralWidget);
+        object->setObjectName(QStringLiteral("object"));
+        object->setGeometry(QRect(660, 110, 68, 21));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setGeometry(QRect(630, 0, 131, 31));
+        pushButton_2 = new QPushButton(centralWidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setGeometry(QRect(230, 290, 131, 31));
+        camera = new QWidget(centralWidget);
+        camera->setObjectName(QStringLiteral("camera"));
+        camera->setGeometry(QRect(0, 0, 581, 291));
+        label = new QLabel(camera);
+        label->setObjectName(QStringLiteral("label"));
+        label->setGeometry(QRect(0, 10, 571, 281));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 825, 27));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
+        object->setText(QString());
+        pushButton->setText(QApplication::translate("MainWindow", "Add New Zone", 0));
+        pushButton_2->setText(QApplication::translate("MainWindow", "Add New Object", 0));
+        label->setText(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
