ff65fd322ba4d290bd2205762cdf9bdca08f97a9
Akul <akul.bector@gmail.com>
2019-10-31 17:53:04 -0400

    coordinate comments

--- Coordinate.cpp
+++ Coordinate.cpp
@@ -1,25 +1,62 @@
+/**
+ * @file Coordinate.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-10-31
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #include "Coordinate.h"
 
+/**
+ * @brief Construct a new Coordinate:: Coordinate object
+ * 
+ * @param x 
+ * @param y 
+ */
 Coordinate::Coordinate(float x, float y)
 {
   xCoord = x;
   yCoord = y;
 };
 
+/**
+ * @brief 
+ * 
+ * @return float 
+ */
 float Coordinate::getx()
 {
   return xCoord;
 }
 
+/**
+ * @brief 
+ * 
+ * @return float 
+ */
 float Coordinate::gety()
 {
   return yCoord;
 }
 
+/**
+ * @brief 
+ * 
+ * @param x 
+ */
 void Coordinate::setX(float x)
 {
   xCoord = x;
 }
+
+/**
+ * @brief 
+ * 
+ * @param y 
+ */
 void Coordinate::setY(float y)
 {
   yCoord = y;
--- Coordinate.h
+++ Coordinate.h
@@ -1,8 +1,22 @@
+/**
+ * @file Coordinate.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-10-31
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #ifndef COORDINATE
 #define COORDINATE
 #include <iostream>
 #include <math.h>
 
+/**
+ * @brief 
+ * 
+ */
 class Coordinate
 {
 private:
