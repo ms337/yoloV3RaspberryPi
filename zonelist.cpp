fe52a2e58b29f0230b5f1128c509d42c81b905bb
Akul <akul.bector@gmail.com>
2019-10-31 17:48:16 -0400

    zonelist comments

--- zonelist.cpp
+++ zonelist.cpp
@@ -1,11 +1,27 @@
+/**
+ * @file zonelist.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-10-31
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #include <iostream>
 #include "zonelist.h"
 #include "ui_zonelist.h"
 #include <QDebug>
 #include <QMessageBox>
 
+
 QString coordsArr[8];
 
+/**
+ * @brief Construct a new Zone List:: Zone List object
+ * 
+ * @param parent 
+ */
 ZoneList::ZoneList(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ZoneList)
@@ -13,11 +29,19 @@ ZoneList::ZoneList(QWidget *parent) :
     ui->setupUi(this);
 }
 
+/**
+ * @brief Destroy the Zone List:: Zone List object
+ * 
+ */
 ZoneList::~ZoneList()
 {
     delete ui;
 }
 
+/**
+ * @brief 
+ * 
+ */
 void ZoneList::on_pushButton_clicked()
 {
     QString coords = ui->textEdit->toPlainText();
@@ -37,6 +61,11 @@ void ZoneList::on_pushButton_clicked()
     }
 }
 
+/**
+ * @brief 
+ * 
+ * @return QString* 
+ */
 QString* ZoneList::getCoords() {
     return coordsArr;
 }
--- zonelist.h
+++ zonelist.h
@@ -1,12 +1,30 @@
+/**
+ * @file zonelist.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-10-31
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #ifndef ZONELIST_H
 #define ZONELIST_H
 
 #include <QDialog>
 
+/**
+ * @brief 
+ * 
+ */
 namespace Ui {
 class ZoneList;
 }
 
+/**
+ * @brief 
+ * 
+ */
 class ZoneList : public QDialog
 {
     Q_OBJECT
