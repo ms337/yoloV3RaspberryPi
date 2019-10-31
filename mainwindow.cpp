5a6d3c9d17fd5e66bbc368a647214b94c6092221
Allen Bao <abao3@uwo.ca>
2019-10-31 15:49:28 -0400

    fixed zone drawer

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,7 +8,8 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),
                                           ui(new Ui::MainWindow)
-                                        
+                               
+                
 {
     ui->setupUi(this);
     QPixmap image("./akul.png");
@@ -31,20 +32,22 @@ void MainWindow::on_pushButton_clicked()
     QPainter painter(&image);
     painter.setPen(QPen(Qt::cyan, 5));
 
-    QString *coordsArr[8];
-    *coordsArr = zonelist.getCoords();
+    QString *coordsArr;
+    coordsArr = zonelist.getCoords();
     
     int intCoords[8];
     
     for (int i = 0; i < 8; i++) {
-        std::cout << coordsArr[i]->toStdString() << std::endl;
+        std::cout << coordsArr[i].toStdString() << std::endl;
         bool ok;
-        intCoords[i] = coordsArr[i]->toInt(&ok, 10);
+        intCoords[i] = coordsArr[i].toInt(&ok, 10);
         //std::cout << intCoords[i] << std::endl;
     }
-    //painter.drawLine(intCoords[0], 50, 100, 150);
+    painter.drawLine(intCoords[0], intCoords[1], intCoords[2], intCoords[3]);
+    painter.drawLine(intCoords[2], intCoords[3], intCoords[6], intCoords[7]);
+    painter.drawLine(intCoords[6], intCoords[7], intCoords[4], intCoords[5]);
+    painter.drawLine(intCoords[4], intCoords[5], intCoords[0], intCoords[1]);
 
-    //2 3 6 7 then 6 7 4 5 then 4 5 0 1
     ui->label->setPixmap(image);
     ui->label->setScaledContents(true);
 }
