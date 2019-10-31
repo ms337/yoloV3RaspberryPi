33f1df7f5da3a9152ca080f8882c27f7db18878b
Akul <akul.bector@gmail.com>
2019-10-31 17:48:48 -0400

    zonecollection comments

--- ZonesCollection.h
+++ ZonesCollection.h
@@ -1,7 +1,22 @@
+/**
+ * @file ZonesCollection.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-10-31
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #ifndef ZONESCOLLECTION
 #define ZONESCOLLECTION
 #include "ZonesCollectionEntry.h"
 #include <iostream>
+
+/**
+ * @brief 
+ * 
+ */
 class ZonesCollection
 {
 private:
