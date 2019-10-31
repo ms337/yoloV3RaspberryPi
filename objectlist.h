3237f59c62481cc8037aa9ae2c07cdb7ae146dbd
Akul <akul.bector@gmail.com>
2019-10-31 17:43:06 -0400

    objectlist comments

--- objectlist.h
+++ objectlist.h
@@ -1,12 +1,30 @@
+/**
+ * @file objectlist.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-10-31
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #ifndef OBJECTLIST_H
 #define OBJECTLIST_H
 
 #include <QDialog>
 
+/**
+ * @brief 
+ * 
+ */
 namespace Ui {
 class ObjectList;
 }
 
+/**
+ * @brief 
+ * 
+ */
 class ObjectList : public QDialog
 {
     Q_OBJECT
