d47ce83a7ef6dcd3b398efddd88051b987193144
Akul <akul.bector@gmail.com>
2019-10-31 17:42:32 -0400

    objectlist comments

--- objectlist.cpp
+++ objectlist.cpp
@@ -1,6 +1,21 @@
+/**
+ * @file objectlist.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-10-31
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #include "objectlist.h"
 #include "ui_objectlist.h"
 
+/**
+ * @brief Construct a new Object List:: Object List object
+ * 
+ * @param parent 
+ */
 ObjectList::ObjectList(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::ObjectList)
@@ -8,6 +23,10 @@ ObjectList::ObjectList(QWidget *parent) :
     ui->setupUi(this);
 }
 
+/**
+ * @brief Destroy the Object List:: Object List object
+ * 
+ */
 ObjectList::~ObjectList()
 {
     delete ui;
