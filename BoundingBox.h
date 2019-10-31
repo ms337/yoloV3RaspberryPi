ee3b54197c2e0b2e7fe08dfe40878b87c1006234
Akul <akul.bector@gmail.com>
2019-10-31 17:55:04 -0400

    boundbox comments

--- BoundingBox.h
+++ BoundingBox.h
@@ -1,7 +1,22 @@
+/**
+ * @file BoundingBox.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-10-31
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #ifndef BOUNDINGBOX
 #define BOUNDINGBOX
 #include "Coordinate.h"
 #include <iostream>
+
+/**
+ * @brief 
+ * 
+ */
 class BoundingBox
 {
 private:
