1698645e7c5ce1d8cdc21b49533393b10c9a81ec
Akul <akul.bector@gmail.com>
2019-10-31 17:52:10 -0400

    displayimage comments

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -1,6 +1,24 @@
+/**
+ * @file DisplayImage.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-10-31
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
 using namespace cv;
+
+/**
+ * @brief 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 int main(int argc, char **argv)
 {
     if (argc != 2)
