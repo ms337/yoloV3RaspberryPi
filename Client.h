609734fe22aa1383ffbf6b4787e86995f28ca3f6
Akul <akul.bector@gmail.com>
2019-10-31 17:53:47 -0400

    client.h comments

--- Client.h
+++ Client.h
@@ -1,3 +1,13 @@
+/**
+ * @file Client.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-10-31
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #ifndef CLIENT
 #define CLIENT
 
@@ -6,6 +16,10 @@
 #include "ObjectsCollection.h"
 #include "Session.h"
 
+/**
+ * @brief 
+ * 
+ */
 class Client
 {
 private:
