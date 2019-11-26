d4cca6a662141a286b8542f0cc6248373ecee53f
jeet undhad <jeetundhad@jeets-MacBook-Pro.local>
2019-11-26 16:40:57 -0500

    added header to modeloutput

--- model/ModelOutput.h
+++ model/ModelOutput.h
@@ -8,16 +8,18 @@
 class ModelOutput
 {
 private:
+  float confThreshold;
+  float nmsThreshold;
+  int inpWidth;
+  int inpHeight;
+  string classesFile;
+  vector<string> classes;
 public:
     ModelOutput(/* args */);
     ~ModelOutput();
     (Mat &frame, const vector<Mat> &outs)
+    void postprocess(Mat& frame, const vector<Mat>& outs);
+    NMSBoxes(boxes, confidences, confThreshold, nmsThreshold, indices);
+    void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame);
+    vector<String> getOutputsNames(const Net& net);
 };
-
-ModelOutput::ModelOutput(/* args */)
-{
-}
-
-ModelOutput::~ModelOutput()
-{
-}
