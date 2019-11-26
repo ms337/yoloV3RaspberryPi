b863f8d6341934ee2badb3842892c38ebadc3364
jeet undhad <jeetundhad@jeets-MacBook-Pro.local>
2019-11-26 14:50:52 -0500

    added ObjectType.h

---
+++ model/ObjectType.h
@@ -0,0 +1,16 @@
+#ifndef OBJECTTYPE
+#define OBJECTTYPE
+
+#include <iostream>
+
+class ObjectType
+{
+private:
+  int i;
+  std::string object[80];
+  std::string selectedObjects[4];
+
+public:
+  std::string getObject(std::string item);
+  void addObject(std::string item);
+};
