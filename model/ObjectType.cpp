ce7525492907e4ecce98e24e910095b2e9d0c929
jeet undhad <jeetundhad@jeets-MacBook-Pro.local>
2019-11-26 00:10:55 -0500

    objecttype now has the array of objects

--- model/ObjectType.cpp
+++ model/ObjectType.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 class objectList{
-
+private:
   int i=0;
   string object[80] = {"person", "bicycle" ,"car","motorcycle","airplane","bus","train","truck","boat","traffic light","fire hydrant","stop_sign","parking meter","bench","bird","cat","dog","horse","sheep","cow","elephant","bear","zebra","giraffe","backpack","umbrella","handbag","tie","suitcase","frisbee","skis","snowboard","sports ball","kite","baseball bat","baseball glove","skateboard","surfboard","tennis racket","bottle","wine glass","cup","fork","knife","spoon","bowl","banana","apple","sandwich","orange","broccoli","carrot","hot dog","pizza","doughnut","cake","chair","couch","potted plant","bed","dining table","toilet","tv","laptop","mouse","remote","keyboard","cell phone","microwave","oven","toaster","sink","refrigerator","book","clock","vase","scissors","teddy bear","hair dryer","toothbrush"};
 
   string selectedObjects[4];
-
+public:
   string getObject(string item){
     int j=0;
     while (item.compare(object[j]) != 0){
