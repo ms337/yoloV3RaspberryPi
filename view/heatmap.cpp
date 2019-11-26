6d044b17c3c88f15c40d6c2f93dd48ac83445abc
Allen Bao <abao3@uwo.ca>
2019-11-26 17:34:44 -0500

    added further heatmap functionality

--- view/heatmap.cpp
+++ view/heatmap.cpp
@@ -7,21 +7,59 @@ Heatmap::Heatmap(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    /**
+     * Input: hash table where key is object and value is an array of ints
+     * Each array will be of length nZones (array index refers to which zone, content of array is occurences of object in zone)
+     * Length of hash table is nObjects
+    **/
+
     int nZones = 4;
-    int nObjects = 5;
+    int nObjects = 3;
+    int maxOccurrences = 10; //set this to largest number in the hash table
+
+    QVector<QString> objects(nObjects);
+    objects[0] = "Person";
+    objects[1] = "Banana";
+    objects[2] = "Poo";
+
+    QVector<double> datax(nZones);
+    for (int i = 0; i < nZones; i++) {
+        datax[i] = i+1;
+    }
+    QVector<double> dataArr[nObjects];
+    for (int i = 0; i < nObjects; i++) {
+        QVector<double> datay(nZones);
+        for (int j = 0; j < nZones; j++) {
+            datay[j] = i+j+20;//rand()%10;
+        }
+        dataArr[i] = datay;
+    }
 
     QCPColorMap *colorMap = new QCPColorMap(ui->widget->xAxis, ui->widget->yAxis);
 
     colorMap->data()->setSize(nObjects, nZones);
-    colorMap->data()->setRange(QCPRange(0, 2), QCPRange(0, 2));
-    for (int x=0; x<nObjects; ++x)
-      for (int y=0; y<nZones; ++y)
-        colorMap->data()->setCell(x, y, y); //rand()%255
-    colorMap->setGradient(QCPColorGradient::gpPolar);
-    colorMap->rescaleDataRange(true);
+    colorMap->data()->setRange(QCPRange(0, nObjects), QCPRange(1, nZones));
+    for (int x=0; x<nObjects; x++)
+      for (int y=0; y<nZones; y++)
+        colorMap->data()->setCell(x+0.5, y+0.5, dataArr[x][y]); //rand()%255
+    colorMap->setGradient(QCPColorGradient::gpHot);
+    //colorMap->rescaleDataRange(true);
     ui->widget->xAxis->setLabel("Objects");
     ui->widget->yAxis->setLabel("Zones");
+
+    QSharedPointer<QCPAxisTickerText> objTicker(new QCPAxisTickerText);
+    for (int i = 0; i < nObjects; i++){
+        objTicker->addTick(i+0.5, objects[i]);
+    }
+    ui->widget->xAxis->setTicker(objTicker);
+
     ui->widget->rescaleAxes();
+
+    QCPColorScale *colorScale = new QCPColorScale(ui->widget);
+    ui->widget->plotLayout()->addElement(0, 1, colorScale);
+    colorScale->setGradient(QCPColorGradient::gpHot);
+    colorScale->setDataRange(QCPRange(0,maxOccurrences));
+
     ui->widget->replot();
 }
 
--- view/histogram.cpp
+++ view/histogram.cpp
@@ -9,35 +9,55 @@ Histogram::Histogram(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    QVector<double> datax = QVector<double>() << 1 << 2 << 3 << 4;
-    QVector<double> dataArr[10];
-    for (int i = 0; i < 3; i++) {
-        QVector<double> datay = QVector<double>() << 0.6 << 0.5 << 0.3 << 0.15;
+    /**
+     * Input: hash table where key is object and value is an array of ints
+     * Each array will be of length nZones (array index refers to which zone, content of array is occurences of object in zone)
+     * Length of hash table is nObjects
+    **/
+
+    int nZones = 5;
+    int nObjects = 6;
+    int maxOccurrences = 10; //set this to largest number in the hash table
+
+    QVector<QString> objects(nObjects);
+    objects[0] = "Person";
+    objects[1] = "Banana";
+    objects[2] = "Poo";
+    objects[3] = "Butt";
+    objects[4] = "Fart";
+    objects[5] = "Dick";
+
+    QVector<double> datax(nZones);
+    for (int i = 0; i < nZones; i++) {
+        datax[i] = i+1;
+    }
+    QVector<double> dataArr[nObjects];
+    for (int i = 0; i < nObjects; i++) {
+        QVector<double> datay(nZones);
+        for (int j = 0; j < nZones; j++) {
+            datay[j] = rand()%10;
+        }
         dataArr[i] = datay;
     }
 
     QCPBarsGroup *group = new QCPBarsGroup(ui->widget);
 
-    QVector<QString> zones(50);
-    zones[0] = "Zone A";
-    zones[1] = "Zone B";
-    zones[2] = "Zone C";
-
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < nObjects; i++) {
         QCPBars *bars = new QCPBars(ui->widget->xAxis, ui->widget->yAxis);
         bars->setData(datax, dataArr[i]);
         bars->setBrush(QColor(rand()%255, rand()%255, rand()%255, 50));
         bars->setPen(QColor(rand()%255, rand()%255, rand()%255));
-        bars->setWidth(0.15);
+        bars->setWidth(1/nObjects);
         bars->setBarsGroup(group);
-        bars->setName(zones[i]);
+        bars->setName(objects[i]);
     }
 
     ui->widget->legend->setVisible(true);
-    ui->widget->xAxis->setRange(0.1, 4.9);
+    ui->widget->xAxis->setRange(0, nZones+1);
     ui->widget->xAxis->setLabel("Zone");
-    ui->widget->yAxis->setRange(0, 0.7);
-    ui->widget->yAxis->setLabel("Number of occurences");
+    ui->widget->yAxis->setRange(0, maxOccurrences+1);
+    ui->widget->yAxis->setLabel("Number of occurrences");
+    //ui->widget->rescaleAxes();
     ui->widget->replot();
 
 }
